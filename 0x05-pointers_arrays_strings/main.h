e8ee4200173b5b7a37d7914fd5ff9ca7c4400d86
akuaaddobea <yohi5991@gmail.com>
2022-09-21 02:36:48 -0700

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,22 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H                                                                                                               
+#define MAIN_H                                                                                                               
+                                                                                                                             
+/**                                                                                                                          
+* void reset_to_98(int *n);                                                                                                  
+* void swap_int(int *a, int *b);                                                                                             
+* int _strlen(char *s);                                                                                                      
+* void _puts(char *str);                                                                                                     
+* void print_rev(char *s);                                                                                                   
+* void rev_string(char *s);                                                                                                  
+* void puts2(char *str);                                                                                                     
+* void puts_half(char *str);                                                                                                 
+* void print_array(int *a, int n);                                                                                           
+* char *_strcpy(char *dest, char *src); 
 
-/**
-* void prototypes()
-* int prototypes ()
-*/
-void reset_to_98(int *n)
-void reset_to_98(int *n);
-void swap_int(int *t *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str, int *b);
-int -strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
 #endif /*MAIN_H*/
