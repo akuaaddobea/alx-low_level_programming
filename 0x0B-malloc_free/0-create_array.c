c6057a3cb1594525ef42c8548928dba70c79651e
akuaaddobea <yohi5991@gmail.com>
2022-10-05 03:44:36 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,13 +17,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char)* size);
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 
 	for (index = 0; index < size; index++)
-		array [index] =c;
+		array[index] = c;
 
 	return (array);
 }
