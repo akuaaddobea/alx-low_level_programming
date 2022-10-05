5e84e5d3f4ed586dc1928945a1c48068da586b16
akuaaddobea <yohi5991@gmail.com>
2022-10-05 15:07:33 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,9 +3,8 @@
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of intergers
- * @width: width of grid 
+ * @width: width of grid
  * @height: height of grid 
- *
  * Return: Nothing
  */
 
@@ -17,19 +16,19 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	alloc_grid = malloc(height *sizeof(int *));
+	alloc_grid = malloc(height * sizeof(int *));
 	if (alloc_grid == NULL)
 	{
 		free(alloc_grid);
 		return (NULL);
 	}
 
-	for (i = 0; i <height; i++)
+	for (i = 0; i < height; i++)
 	{
 		alloc_grid[i] = malloc(width * sizeof(int));
 		if (alloc_grid[i] == NULL)
 	{
-		for (i--; i >= 0; i--)
+		for (i--; i > = 0; i--)
 			free(alloc_grid[i]);
 		free(alloc_grid);
 		return (NULL);
