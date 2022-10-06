88ede62cf79a91bad8176d927d11c4ddb97e228a
akuaaddobea <yohi5991@gmail.com>
2022-10-05 22:10:03 -0700

    Task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,12 +5,12 @@
  * free_grid - prints a grid of intergers
  * @grid: the address of the two dimensional grid
  * @width: width of the grid
- * @height: height of the grid 
+ * @height: height of the grid
  * Return: Nothing
  */
 void free_grid(int **grid, int height)
-{ 
-	if (grid != NULL && height !=0)
+{
+	if (grid != NULL && height != 0)
 	{
 		for (; height >= 0; height--)
 			free(grid[height]);
